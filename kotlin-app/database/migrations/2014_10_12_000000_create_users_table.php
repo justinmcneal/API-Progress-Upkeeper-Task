53f87d1c55f7fd0c75876539e846e6b377d48baa
LA-Reyes <163097815+LA-Reyes@users.noreply.github.com>
2024-09-26 22:08:04 +0800

    small update on users table

--- kotlin-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ kotlin-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->binary('profile')->nullable();
+            //$table->binary('profile')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
